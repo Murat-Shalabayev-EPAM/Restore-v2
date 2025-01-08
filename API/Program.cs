12698bcdcc8f8fbd83a625e2e8de52a3d801fe88
Murat Shalabayev <Murat_Shalabayev@epam.com>
2025-01-08 18:52:38 +0500

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,8 +9,16 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(options => {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(opt => 
+{
+    opt.AllowAnyHeader()
+       .AllowAnyMethod()
+       .WithOrigins("https://localhost:3000");
+});
 
 app.MapControllers();
 
