79d102d2f7ddcdf6cc976a35d67cc7ef29eef5b5
Murat Shalabayev <Murat_Shalabayev@epam.com>
2025-01-14 11:47:33 +0500

    End of section 6

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,9 +11,11 @@ builder.Services.AddDbContext<StoreContext>(options => {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddTransient<ExceptionMiddleware>();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(opt => 
 {
     opt.AllowAnyHeader()
--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,3 +1,86 @@
+import {
+  Alert,
+  Button,
+  ButtonGroup,
+  Container,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
+import {
+  useLazyGet400ErrorQuery,
+  useLazyGet401ErrorQuery,
+  useLazyGet404ErrorQuery,
+  useLazyGet500ErrorQuery,
+  useLazyGetValidationErrorQuery,
+} from "./errorApi";
+import { useState } from "react";
+
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  const [validationErrors, setValidationErrors] = useState<string[]>([]);
+  const [trigger400Error] = useLazyGet400ErrorQuery();
+  const [trigger401Error] = useLazyGet401ErrorQuery();
+  const [trigger404Error] = useLazyGet404ErrorQuery();
+  const [trigger500Error] = useLazyGet500ErrorQuery();
+  const [triggerValidationError] = useLazyGetValidationErrorQuery();
+  const getValidationError = async () => {
+    try {
+      await triggerValidationError().unwrap();
+    } catch (error: unknown) {
+      if (
+        error &&
+        typeof error === "object" &&
+        "message" in error &&
+        typeof (error as { message: unknown }).message === "string"
+      ) {
+        const errorArray = (error as { message: string }).message.split(",");
+        setValidationErrors(errorArray);
+      }
+    }
+  };
+  return (
+    <Container maxWidth="lg">
+      <Typography gutterBottom variant="h3">
+        Errors with testing
+      </Typography>
+      <ButtonGroup fullWidth>
+        <Button
+          variant="contained"
+          onClick={() => trigger400Error().catch((err) => console.log(err))}
+        >
+          400 error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => trigger401Error().catch((err) => console.log(err))}
+        >
+          401 error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => trigger404Error().catch((err) => console.log(err))}
+        >
+          404 error
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => trigger500Error().catch((err) => console.log(err))}
+        >
+          500 error
+        </Button>
+        <Button variant="contained" onClick={getValidationError}>
+          Validation error
+        </Button>
+      </ButtonGroup>
+      {validationErrors.length > 0 && (
+        <Alert severity="error">
+          <List>
+            {validationErrors.map((err) => (
+              <ListItem key={err}>{err}</ListItem>
+            ))}
+          </List>
+        </Alert>
+      )}
+    </Container>
+  );
 }
